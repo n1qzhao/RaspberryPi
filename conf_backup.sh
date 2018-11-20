316081f9ea35c130c531e22d7d3e179bb851ec6b
Garydz <gary.dzhao@outlook.com>
2018-11-19 23:50:59 -0600

    initial release

---
+++ conf_backup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Program:
+#  Backup files specified in SCRIPTNAME.conf and user's crontab
+#  to folder name specified in SCRIPTNAME.conf or commandline
+# Version:
+#  20181118 - First release
+
+conf=$(dirname ${0})/$(basename ${0} .sh).conf
+if [ ! -f ${conf} ]; then
+	echo "${conf} not found! Creating a basic one..."
+	touch ${conf}
+	echo "# Where should the files be backup," >> ${conf}
+	echo "# the path must be full path end with /" >> ${conf}
+	echo "bakFolderRoot=\"/home/pi/conf_bak/\"" >> ${conf}
+	echo "#" >> ${conf}
+	echo "# Which files or folders will be backup," >> ${conf}
+	echo "# must be full path from /" >> ${conf}
+	echo "bakFiles=(\"/home/pi/.bashrc\" \"/home/pi/bin/\" \"/etc/fstab\")" >> ${conf}
+	echo "#" >> ${conf}
+fi
+. ${conf}
+
+if [ ${#} -gt 0 ]; then
+	bakFolderRoot=${1}
+fi
+
+bakFolder=${bakFolderRoot}conf_$(date +%Y%m%d%H%M%S)
+echo "All files backup to ${bakFolder}"
+
+for f in ${bakFiles[*]}
+do
+	if [ -d ${f} ]; then
+		echo "Backup dir \"${f}\" "
+		mkdir -p ${bakFolder}$(dirname ${f})
+		cp -r ${f} ${bakFolder}$(dirname ${f})
+	elif [ -f ${f} ]; then
+		echo "Backup file \"${f}\" "
+		mkdir -p ${bakFolder}$(dirname ${f})
+		cp ${f} ${bakFolder}$(dirname ${f})
+	else
+		echo "Item \"${f}\" is not backable"
+	fi
+done
+
+echo "Backup your crontab"
+crontab -l > ${bakFolder}/crontab
+
+exit 0
---
+++ conf_restore.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# Program:
+#  A reverse operation of conf_backup.sh
+#  Restore all files and subfolders in current current folder
+#  or path specified in commandline
+#  to ${toRoot}
+# Version:
+#  20181119 - First release
+
+conf=$(dirname ${0})/$(basename ${0} .sh).conf
+if [ ! -f ${conf} ]; then
+        echo "${conf} not found! Creating a basic one..."
+	touch ${conf}
+	echo "# Where should the files be put back," >> ${conf}
+	echo "# the path must be full path end with /" >> ${conf}
+        echo "toRoot=\"/home/pi/test/\"" >> ${conf}
+	echo "#" >> ${conf}
+fi
+. ${conf}
+
+if [ ${#} -gt 0 ]; then
+	wd=${1}
+else
+	wd=$(pwd)
+fi
+
+echo "Restoring files in ${wd}, you need be root to restore system files!"
+read -n 1 -p "All existing files will be overwritten! Continue?[Yes/no]: " c
+case ${c} in
+	n|N)
+		exit 0
+		;;
+	*)
+		;;
+esac
+
+if [ -d ${wd} ]; then
+	cd ${wd}
+else
+	echo "${wd} is not a folder! Stopped!"
+	exit 1
+fi
+
+if [ ! -d ${toRoot} ]; then
+	mkdir -p ${toRoot}
+fi
+
+files=`find * | grep -v "crontab"`
+
+for f in ${files[*]}
+do
+	if [ -d ${f} ]; then
+		echo "mkdir ${toRoot}${f}"
+		mkdir -p ${toRoot}${f}
+	elif [ -f ${f} ]; then
+		echo "cp ${f}"
+		cp ${f} ${toRoot}${f}
+	fi
+done
+
+if [ -f "crontab" ]; then
+	crontab crontab
+	if [ ${?} -eq 0 ]; then
+		echo -e "\nAll files and your crontab restored!"
+	else
+		echo -e "\nAll files restored!\nDon't forget your crontab!"
+	fi
+fi
+
+exit 0
